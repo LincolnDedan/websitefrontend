ae4f730addf29b083b046eb91929a8a5dd7fb4dd
Northrift.git <peterbarasa600@gmail.com>
2024-03-14 21:54:20 +0300

    register form

---
+++ Datatypes.py
@@ -0,0 +1,8 @@
+a=100
+print ("The type of variable having value", a," is " type(a))
+print (a)
+b=True
+print (b)
+print ("The type of variable having value", b, " is ", type(b))
+c = 20.345
+d = 10+3j print ("The type of variable having value", c, " is ", type(c))
